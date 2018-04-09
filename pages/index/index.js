11f6fc33f87b24d4d598ec9f569ee223fa630e20
xenonflash <jzykc@hotmail.com>
2018-04-10 00:16:44 +0800

    add tab

--- pages/index/index.js
+++ pages/index/index.js
@@ -2,4 +2,46 @@
 //获取应用实例
 const app = getApp()
 
-Page({})
+const newsCategory = [
+  {
+    id: 'gn',
+    name: '国内'
+  },
+  {
+    id: 'gj',
+    name: '国际'
+  },
+  {
+    id: 'cj',
+    name: '财经'
+  },
+  {
+    id: 'yl',
+    name: '娱乐'
+  },
+  {
+    id: 'js',
+    name: '纪实'
+  },
+  {
+    id: 'ty',
+    name: '台湾'
+  },
+  {
+    id: 'other',
+    name: '综合'
+  },
+]
+
+Page({
+  data: {
+    newsCategory,
+    currCate: 0
+  },
+  handleCateChange(e) {
+    const index = e.target.dataset.index
+    this.setData({
+      currCate: index
+    })
+  }
+})
